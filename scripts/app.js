80a79e076e203a69027c6fd9295f867e414d1bfe
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-01 00:19:02 -0400

    add attack functionality with game end scenario

--- scripts/app.js
+++ scripts/app.js
@@ -10,6 +10,29 @@ new Vue({
             this.playerHealth = 100;
             this.monsterHealth = 100;
             this.gameStarted = true;
+        },
+        restartGame: function() {
+            this.startGame();
+            this.gameStarted = false;
+        },
+        attack: function() {
+            this.playerHealth -= Math.floor(Math.random()*(11)+5);
+            this.monsterHealth -= Math.floor(Math.random()*(11)+5);
+            this.checkForWin();
+        },
+        giveUp: function() {
+            alert('You gave up!');
+            this.restartGame();
+        },
+        checkForWin: function() {
+            if (this.playerHealth <= 0) {
+                alert('You lost!');
+                this.restartGame();
+            }
+            if (this.monsterHealth <= 0) {
+                alert('You won!');
+                this.restartGame();
+            }
         }
     }
 })
