3e51b7d2e3ae69fc8bf24dd45bc630b785aad8e7
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-05-31 22:22:13 -0400

    added starting game mechanisms for player/monster health bar and start button

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,15 @@
 new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        gameStarted: false,
+        playerHealth: 100,
+        monsterHealth: 100
+    },
+    methods: {
+        startGame: function() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.gameStarted = true;
+        }
+    }
 })
