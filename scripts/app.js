9988a3ef19fbd195a0f920fc4f58aae56c80e04c
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-01 20:04:28 -0400

    complete business logic, handling, and interface

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         gameStarted: false,
         playerHealth: 100,
-        monsterHealth: 100
+        monsterHealth: 100,
+        battleLog: []
     },
     methods: {
         startGame: function() {
@@ -14,17 +15,52 @@ new Vue({
         restartGame: function() {
             this.startGame();
             this.gameStarted = false;
+            this.battleLog = [];
+        },
+        calculateDamage: function(max, min) {
+            return Math.floor(Math.random()*(max - min + 1) + min);
+        },
+        tradeAttacks: function(playerHit, monsterHit) {
+            this.playerHealth -= playerHit;
+            this.monsterHealth -= monsterHit;
+            this.battleLog.unshift({
+                monster: 'Monster hits player for ' + playerHit,
+                player: 'Player hits monster for ' + monsterHit
+            });
         },
         attack: function() {
-            this.playerHealth -= Math.floor(Math.random()*(11)+5);
-            this.monsterHealth -= Math.floor(Math.random()*(11)+5);
-            this.checkForWin();
+            var playerHit = this.calculateDamage(15, 5);
+            var monsterHit = this.calculateDamage(15, 5);
+            this.tradeAttacks(playerHit, monsterHit);
+            this.checkWinLoss();
+        },
+        specialAttack: function() {
+            var playerHit = this.calculateDamage(20, 10);
+            var monsterHit = this.calculateDamage(20, 10);
+            this.tradeAttacks(playerHit, monsterHit);
+            this.checkWinLoss();
+        },
+        healUp: function() {
+            if (this.playerHealth < 80) {
+                var playerHit = this.calculateDamage(20, 10);
+                var playerHeal = this.calculateDamage(20, 10);
+                this.playerHealth += playerHeal;
+                this.playerHealth -= playerHit;
+                this.battleLog.unshift({
+                    monster: 'Monster hits player for ' + playerHit,
+                    player: 'Player heals himself for ' + playerHeal
+                });
+                this.checkWinLoss();
+            }
+            else {
+                alert('You can\'t heal if your health is 80% of above!');
+            }
         },
         giveUp: function() {
             alert('You gave up!');
             this.restartGame();
         },
-        checkForWin: function() {
+        checkWinLoss: function() {
             if (this.playerHealth <= 0) {
                 alert('You lost!');
                 this.restartGame();
@@ -35,4 +71,4 @@ new Vue({
             }
         }
     }
-})
+});
